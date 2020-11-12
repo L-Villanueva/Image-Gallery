3d59da281fbf62f6ad0eed2ac08c7814c9887dea
Luis Villanueva <luisvillanueva97@gmail.com>
2020-11-12 14:58:03 +0100

    Added Album Cell

---
+++ Image Gallery/Image Gallery/AlbumCell.swift
@@ -0,0 +1,20 @@
+//
+//  AlbumCell.swift
+//  Image Gallery
+//
+//  Created by Luis Villanueva on 12/11/2020.
+//
+
+import UIKit
+
+class AlbumCell: UICollectionViewCell {
+
+    @IBOutlet weak var cellView: UIView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- Image Gallery/Image Gallery/ViewController.swift
+++ Image Gallery/Image Gallery/ViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+   
+    
 
+    @IBOutlet weak var albumCollectionView: UICollectionView!
+    
+    override func loadView() {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        albumCollectionView.delegate = self
+        albumCollectionView.dataSource = self
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
     }
 }
 
